9ef4e08ff278f42eba04e744d8299d4293fbcbc5
aldisuryap <appleimport123@gmail.com>
2020-03-30 05:52:31 +0700

    Fixing camera bug, adding map stage Lvl1, Adding collecting item

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target;
+    public float smoothing;
+    public Vector2 minPosition;
+    public Vector2 maxPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+        // transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+
+        if (transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+        }
     }
 }
---
+++ Assets/Scripts/CrystalCount.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrystalCount : MonoBehaviour
+{
+    public Text crystalText;
+    private int total = 0;
+    void Start()
+    {
+        UpdateCrystalText();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D hit)
+    {
+        if (hit.CompareTag("Crystal"))
+        {
+            total++;
+            UpdateCrystalText();
+            Destroy(hit.gameObject);
+        }
+    }
+
+    private void UpdateCrystalText()
+    {
+        string msg = "" + total;
+        crystalText.text = msg;
+    }
+}
