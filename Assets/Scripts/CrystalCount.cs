005d8fd0888823db6e2b59863f64e933c8c29544
aldisuryap <appleimport123@gmail.com>
2020-03-30 12:43:12 +0700

    Adding collecting chest for instant win

--- Assets/Scripts/CrystalCount.cs
+++ Assets/Scripts/CrystalCount.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class CrystalCount : MonoBehaviour
 {
     public Text crystalText;
+    public GameObject winStage;
     private int total = 0;
+    private int totalChest = 0;
     void Start()
     {
         UpdateCrystalText();
@@ -21,11 +23,23 @@ public class CrystalCount : MonoBehaviour
             UpdateCrystalText();
             Destroy(hit.gameObject);
         }
+        else if (hit.CompareTag("Chest"))
+        {
+            winStage.SetActive(true);
+            Destroy(hit.gameObject);
+            gameObject.SetActive(false);
+        }
     }
 
     private void UpdateCrystalText()
     {
         string msg = "" + total;
         crystalText.text = msg;
+
+        if (total == 1)
+        {
+            winStage.SetActive(true);
+            gameObject.SetActive(false);
+        }
     }
 }
